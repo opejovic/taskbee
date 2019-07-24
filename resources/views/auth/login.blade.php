a3394d13904ccad83210c3211bc3603f5fe696cc
Ognjen P <opejovic@gmail.com>
2019-07-24 16:10:58 +0200

    Is it working?

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,18 +21,17 @@
                 @csrf
                 <div class="flex flex-wrap -mx-3 mb-1">
                     <div class="w-full md:w-3/3 px-3">
-                        <input
+						<input
                             class="shadow appearance-none block w-full bg-gray-200 text-center text-gray-700 border @error('email') border-red-700 @enderror rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                             id="email"
                             type="email"
                             name="email"
                             placeholder="Email"
-                            required
                             value="{{ old('email') }}"
                         />
 
                         @error('email')
-                        <p class="text-red-600 text-xs -mt-2">{{ $message }}</p>
+                        	<p class="text-red-600 text-xs -mt-2">{{ $message }}</p>
                         @enderror
                     </div>
                 </div>
@@ -44,11 +43,10 @@
                             type="password"
                             name="password"
                             placeholder="Password"
-                            required
                         />
 
                         @error('password')
-                        <p class="text-red-600 text-xs">{{ $message }}</p>
+                        	<p class="text-red-600 text-xs">{{ $message }}</p>
                         @enderror
                     </div>
                 </div>
