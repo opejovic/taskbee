6bd46dc3ebffef581d402a99f1e51c4ca8ef899d
Ognjen <opejovic@gmail.com>
2019-12-04 20:23:56 +0100

    Update services.php

--- config/services.php
+++ config/services.php
@@ -35,7 +35,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\Models\User::class,
+        'model' => taskbee\Models\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
         'webhook' => [
