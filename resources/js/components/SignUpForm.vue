c03057af133f7fc82a379a49bc75204a5ca702a8
Ognjen <opejovic@gmail.com>
2020-04-16 02:55:15 +0200

    Redesign Register page.

--- resources/js/components/SignUpForm.vue
+++ resources/js/components/SignUpForm.vue
@@ -1,38 +1,29 @@
 <template>
-  <div
-    class="flex w-full mx-auto items-center px-3 pb-10"
-  >
+  <div class="flex w-full mx-auto items-center px-3 pb-10">
     <!-- if user is signed in already, dont show the form for registraton, else, show the subscription form. -->
     <div class="mx-auto w-full">
       <header class="flex justify-between items-center py-4">
         <a href="/" class="flex items-center">
           <img class="w-12" src="/img/logo.svg" alt="Imaginary" />
-          <p class="pl-1 font-bold text-xl tracking-tighter text-indigo-900">
-            taskbee.
-          </p>
+          <p class="pl-1 font-bold text-xl tracking-tighter text-indigo-900">taskbee.</p>
         </a>
         <a
           href="/login"
           class="block mt-4 text-sm lg:inline-block lg:mt-0 text-indigo-800 hover:text-indigo-500 border-b-2 border-transparent hover:border-indigo-500"
-        >
-          Sign in
-        </a>
+        >Sign in</a>
       </header>
-      <div class="pt-5 pb-5 text-center">
-        <div class="w-full py-8 curve-pattern rounded-lg">
-          <span class="font-light text-2xl flex-1 text-indigo-900"
-            >Create a workspace for your team, and start assigning tasks
-            today.</span
-          >
+      <div class="pt-5 pb-5 text-center pb-10 w-full overflow-x-hidden">
+        <div class="py-8 rounded-lg">
+          <div style="position: relative !important;" class="sign-up-pattern font-light text-2xl flex font-mono text-indigo-900 py-4 mx-auto">
+            Create a workspace for your team, and start assigning tasks today.
+          </div>
         </div>
       </div>
       <div class="flex flex-wrap mx-auto">
         <div class="xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-full mx-auto">
           <div class="mb-3 text-center">
             <div class="w-full">
-              <span class="text-sm uppercase tracking-tight text-indigo-900"
-                >Choose a plan</span
-              >
+              <span class="text-sm uppercase tracking-tight text-indigo-900">Choose a plan</span>
             </div>
           </div>
           <div v-for="plan in plans" :key="plan.id" style="cursor: pointer;" class="px-3">
@@ -49,25 +40,22 @@
                     <i
                       class="material-icons indie-text ml-auto"
                       v-if="clicked(plan.id)"
-                      >check_circle_outline</i
-                    >
+                    >check_circle_outline</i>
                   </div>
                 </div>
                 <div class="flex items-center">
                   <div class="text-gray-700 mr-auto tracking-tighter">
                     <span class="font-semibold text-2xl">
                       €
-                      {{ (plan.amount * plan.members_limit) / 100 }}</span
-                    >
-                    <span class="text-xl font-light">
-                      / month
+                      {{ (plan.amount * plan.members_limit) / 100 }}
                     </span>
+                    <span class="text-xl font-light">/ month</span>
                   </div>
                   <div class="text-gray-700 text-left w-1/3">
-                    <span class="text-sm"
-                      >{{ plan.members_limit }} member slots. Unlimited storage,
-                      and more...</span
-                    >
+                    <span class="text-sm">
+                      {{ plan.members_limit }} member slots. Unlimited storage,
+                      and more...
+                    </span>
                   </div>
                 </div>
               </div>
@@ -78,9 +66,7 @@
         <div class="xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-full mx-auto px-3">
           <div class="mb-3 text-center">
             <div class="w-full">
-              <span class="text-sm uppercase tracking-tight text-indigo-900"
-                >Account details</span
-              >
+              <span class="text-sm uppercase tracking-tight text-indigo-900">Account details</span>
             </div>
           </div>
           <div class="w-full">
@@ -106,7 +92,7 @@
                   v-text="form.errors.get('first_name')"
                 ></p>
               </div>
-              <div class=" mb-1">
+              <div class="mb-1">
                 <div class="w-full">
                   <input
                     type="text"
@@ -129,7 +115,7 @@
                   ></p>
                 </div>
               </div>
-              <div class=" mb-1">
+              <div class="mb-1">
                 <div class="w-full">
                   <input
                     type="email"
@@ -150,7 +136,7 @@
                   ></p>
                 </div>
               </div>
-              <div class=" mb-1">
+              <div class="mb-1">
                 <div class="w-full">
                   <input
                     type="email"
@@ -164,7 +150,7 @@
                   />
                 </div>
               </div>
-              <div class=" mb-1">
+              <div class="mb-1">
                 <div class="w-full">
                   <input
                     type="password"
@@ -187,7 +173,7 @@
                   ></p>
                 </div>
               </div>
-              <div class=" mb-1">
+              <div class="mb-1">
                 <div class="w-full">
                   <input
                     type="password"
@@ -212,7 +198,7 @@
                   ></p>
                 </div>
               </div>
-              <div class="">
+              <div class>
                 <div class="w-full">
                   <button
                     class="block w-full uppercase mx-auto shadow border-b-4 border-purple-700 bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded"
@@ -233,115 +219,111 @@
 </template>
 
 <script>
-  export default {
-    props: ["plans"],
-    data() {
-      return {
-        form: new Form({
-          first_name: null,
-          last_name: null,
-          email: null,
-          email_confirmation: null,
-          password: null,
-          password_confirmation: null
-        }),
+export default {
+  props: ["plans"],
+  data() {
+    return {
+      form: new Form({
+        first_name: null,
+        last_name: null,
+        email: null,
+        email_confirmation: null,
+        password: null,
+        password_confirmation: null
+      }),
 
-        processing: false,
-        spinning: false,
-        selectedPlan: 1 // by default the selected plan is the plan with the id of 1.
-      };
-    },
+      processing: false,
+      spinning: false,
+      selectedPlan: 1 // by default the selected plan is the plan with the id of 1.
+    };
+  },
 
-    computed: {
-      state() {
-        return this.processing ? "PROCESSING... " : "PROCEED TO CHECKOUT";
-      }
-    },
-
-    methods: {
-      choose(plan) {
-        this.selectedPlan = plan.id;
-      },
-
-      clicked(id) {
-        return this.selectedPlan === id ? true : false;
-      },
-
-      register() {
-        this.form
-          .post("/register")
-          .then(response => {
-            // create a customer and then proceed to checkout with the customer
-            this.checkout();
-          })
-          .catch(error => {
-            console.log(error)
-          });
-      },
-
-      checkout() {
-        const stripe = Stripe(process.env.MIX_STRIPE_KEY);
-
-        this.processing = true;
-
-        axios.post(`/plans/${this.selectedPlan}/checkout`, {}).then(response => {
-          stripe
-            .redirectToCheckout({
-              // Make the id field from the Checkout Session creation API response
-              // available to this file, so you can provide it as parameter here
-              // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-              sessionId: response.data.id
-            })
-            .then(result => {
-              // If `redirectToCheckout` fails due to a browser or network
-              // error, display the localized error message to your customer
-              // using `result.error.message`.
-            });
-        });
-      }
+  computed: {
+    state() {
+      return this.processing ? "PROCESSING... " : "PROCEED TO CHECKOUT";
     }
-  };
+  },
+
+  methods: {
+    choose(plan) {
+      this.selectedPlan = plan.id;
+    },
+
+    clicked(id) {
+      return this.selectedPlan === id ? true : false;
+    },
+
+    register() {
+      this.form
+        .post("/register")
+        .then(response => {
+          // create a customer and then proceed to checkout with the customer
+          this.checkout();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+
+    checkout() {
+      const stripe = Stripe(process.env.MIX_STRIPE_KEY);
+
+      this.processing = true;
+
+      axios.post(`/plans/${this.selectedPlan}/checkout`, {}).then(response => {
+        stripe
+          .redirectToCheckout({
+            // Make the id field from the Checkout Session creation API response
+            // available to this file, so you can provide it as parameter here
+            // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+            sessionId: response.data.id
+          })
+          .then(result => {
+            // If `redirectToCheckout` fails due to a browser or network
+            // error, display the localized error message to your customer
+            // using `result.error.message`.
+          });
+      });
+    }
+  }
+};
 </script>
 
 <style>
-  .indie-border {
-    border-color: rgb(82, 0, 177);
-    border-width: 2px;
-  }
+.indie-border {
+  border-color: rgb(82, 0, 177);
+  border-width: 2px;
+}
 
-  .unclickable {
-    pointer-events: none;
-  }
+.unclickable {
+  pointer-events: none;
+}
 
-  .indie-text {
-    color: rgb(82, 0, 177);
-  }
+.indie-text {
+  color: rgb(82, 0, 177);
+}
 
-  .indie-btn {
-    background-color: rgb(82, 0, 177);
-    border-color: rgb(82, 0, 177);
-    color: rgb(255, 255, 255);
-  }
+.indie-btn {
+  background-color: rgb(82, 0, 177);
+  border-color: rgb(82, 0, 177);
+  color: rgb(255, 255, 255);
+}
 
-  .indie-btn.disabled,
-  .indie-btn:disabled {
-    background-color: rgb(82, 0, 177);
-    border-color: rgb(82, 0, 177);
-    color: rgb(255, 255, 255);
-  }
+.indie-btn.disabled,
+.indie-btn:disabled {
+  background-color: rgb(82, 0, 177);
+  border-color: rgb(82, 0, 177);
+  color: rgb(255, 255, 255);
+}
 
-  .indie-btn:hover {
-    background-color: rgb(135, 74, 211);
-    border-color: rgb(135, 74, 211);
-    color: rgb(255, 255, 255);
-  }
+.indie-btn:hover {
+  background-color: rgb(135, 74, 211);
+  border-color: rgb(135, 74, 211);
+  color: rgb(255, 255, 255);
+}
 
-  .indie-btn-loading {
-    background-color: rgb(135, 74, 211);
-    border-color: rgb(135, 74, 211);
-  }
-
-  .curve-pattern {
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 280 280' width='280' height='280'%3E%3Cpath fill='%239C92AC' fill-opacity='0.08' d='M44.1 224a5 5 0 1 1 0 2H0v-2h44.1zm160 48a5 5 0 1 1 0 2H82v-2h122.1zm57.8-46a5 5 0 1 1 0-2H304v2h-42.1zm0 16a5 5 0 1 1 0-2H304v2h-42.1zm6.2-114a5 5 0 1 1 0 2h-86.2a5 5 0 1 1 0-2h86.2zm-256-48a5 5 0 1 1 0 2H0v-2h12.1zm185.8 34a5 5 0 1 1 0-2h86.2a5 5 0 1 1 0 2h-86.2zM258 12.1a5 5 0 1 1-2 0V0h2v12.1zm-64 208a5 5 0 1 1-2 0v-54.2a5 5 0 1 1 2 0v54.2zm48-198.2V80h62v2h-64V21.9a5 5 0 1 1 2 0zm16 16V64h46v2h-48V37.9a5 5 0 1 1 2 0zm-128 96V208h16v12.1a5 5 0 1 1-2 0V210h-16v-76.1a5 5 0 1 1 2 0zm-5.9-21.9a5 5 0 1 1 0 2H114v48H85.9a5 5 0 1 1 0-2H112v-48h12.1zm-6.2 130a5 5 0 1 1 0-2H176v-74.1a5 5 0 1 1 2 0V242h-60.1zm-16-64a5 5 0 1 1 0-2H114v48h10.1a5 5 0 1 1 0 2H112v-48h-10.1zM66 284.1a5 5 0 1 1-2 0V274H50v30h-2v-32h18v12.1zM236.1 176a5 5 0 1 1 0 2H226v94h48v32h-2v-30h-48v-98h12.1zm25.8-30a5 5 0 1 1 0-2H274v44.1a5 5 0 1 1-2 0V146h-10.1zm-64 96a5 5 0 1 1 0-2H208v-80h16v-14h-42.1a5 5 0 1 1 0-2H226v18h-16v80h-12.1zm86.2-210a5 5 0 1 1 0 2H272V0h2v32h10.1zM98 101.9V146H53.9a5 5 0 1 1 0-2H96v-42.1a5 5 0 1 1 2 0zM53.9 34a5 5 0 1 1 0-2H80V0h2v34H53.9zm60.1 3.9V66H82v64H69.9a5 5 0 1 1 0-2H80V64h32V37.9a5 5 0 1 1 2 0zM101.9 82a5 5 0 1 1 0-2H128V37.9a5 5 0 1 1 2 0V82h-28.1zm16-64a5 5 0 1 1 0-2H146v44.1a5 5 0 1 1-2 0V18h-26.1zm102.2 270a5 5 0 1 1 0 2H98v14h-2v-16h124.1zM242 149.9V160h16v34h-16v62h48v48h-2v-46h-48v-66h16v-30h-16v-12.1a5 5 0 1 1 2 0zM53.9 18a5 5 0 1 1 0-2H64V2H48V0h18v18H53.9zm112 32a5 5 0 1 1 0-2H192V0h50v2h-48v48h-28.1zm-48-48a5 5 0 0 1-9.8-2h2.07a3 3 0 1 0 5.66 0H178v34h-18V21.9a5 5 0 1 1 2 0V32h14V2h-58.1zm0 96a5 5 0 1 1 0-2H137l32-32h39V21.9a5 5 0 1 1 2 0V66h-40.17l-32 32H117.9zm28.1 90.1a5 5 0 1 1-2 0v-76.51L175.59 80H224V21.9a5 5 0 1 1 2 0V82h-49.59L146 112.41v75.69zm16 32a5 5 0 1 1-2 0v-99.51L184.59 96H300.1a5 5 0 0 1 3.9-3.9v2.07a3 3 0 0 0 0 5.66v2.07a5 5 0 0 1-3.9-3.9H185.41L162 121.41v98.69zm-144-64a5 5 0 1 1-2 0v-3.51l48-48V48h32V0h2v50H66v55.41l-48 48v2.69zM50 53.9v43.51l-48 48V208h26.1a5 5 0 1 1 0 2H0v-65.41l48-48V53.9a5 5 0 1 1 2 0zm-16 16V89.41l-34 34v-2.82l32-32V69.9a5 5 0 1 1 2 0zM12.1 32a5 5 0 1 1 0 2H9.41L0 43.41V40.6L8.59 32h3.51zm265.8 18a5 5 0 1 1 0-2h18.69l7.41-7.41v2.82L297.41 50H277.9zm-16 160a5 5 0 1 1 0-2H288v-71.41l16-16v2.82l-14 14V210h-28.1zm-208 32a5 5 0 1 1 0-2H64v-22.59L40.59 194H21.9a5 5 0 1 1 0-2H41.41L66 216.59V242H53.9zm150.2 14a5 5 0 1 1 0 2H96v-56.6L56.6 162H37.9a5 5 0 1 1 0-2h19.5L98 200.6V256h106.1zm-150.2 2a5 5 0 1 1 0-2H80v-46.59L48.59 178H21.9a5 5 0 1 1 0-2H49.41L82 208.59V258H53.9zM34 39.8v1.61L9.41 66H0v-2h8.59L32 40.59V0h2v39.8zM2 300.1a5 5 0 0 1 3.9 3.9H3.83A3 3 0 0 0 0 302.17V256h18v48h-2v-46H2v42.1zM34 241v63h-2v-62H0v-2h34v1zM17 18H0v-2h16V0h2v18h-1zm273-2h14v2h-16V0h2v16zm-32 273v15h-2v-14h-14v14h-2v-16h18v1zM0 92.1A5.02 5.02 0 0 1 6 97a5 5 0 0 1-6 4.9v-2.07a3 3 0 1 0 0-5.66V92.1zM80 272h2v32h-2v-32zm37.9 32h-2.07a3 3 0 0 0-5.66 0h-2.07a5 5 0 0 1 9.8 0zM5.9 0A5.02 5.02 0 0 1 0 5.9V3.83A3 3 0 0 0 3.83 0H5.9zm294.2 0h2.07A3 3 0 0 0 304 3.83V5.9a5 5 0 0 1-3.9-5.9zm3.9 300.1v2.07a3 3 0 0 0-1.83 1.83h-2.07a5 5 0 0 1 3.9-3.9zM97 100a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-48 32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 48a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-64a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 96a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-144a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-96 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm96 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-64a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-32 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM49 36a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-32 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM33 68a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-48a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 240a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-64a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm80-176a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 48a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm112 176a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM17 180a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM17 84a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 64a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6z'%3E%3C/path%3E%3C/svg%3E");
-  }
+.indie-btn-loading {
+  background-color: rgb(135, 74, 211);
+  border-color: rgb(135, 74, 211);
+}
 </style>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,58 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Create your account</div>
 
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">Create your account</div>
-
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}">
-                            @csrf
-
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label for="first_name">First name</label>
-                                    <input type="text" class="form-control" name="first_name" id="first_name"
-                                           placeholder="Enter your first name">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="last_name">Last name</label>
-                                    <input type="text" class="form-control" name="last_name" id="last_name"
-                                           placeholder="Enter your last name">
-                                </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="first_name">First name</label>
+                                <input type="text" class="form-control" name="first_name" id="first_name"
+                                    placeholder="Enter your first name">
                             </div>
-                            <div class="form-group">
-                                <label for="email">Email address</label>
-                                <input type="email" class="form-control" name="email" id="email"
-                                       aria-describedby="emailHelp" placeholder="Enter email">
+                            <div class="form-group col-md-6">
+                                <label for="last_name">Last name</label>
+                                <input type="text" class="form-control" name="last_name" id="last_name"
+                                    placeholder="Enter your last name">
                             </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control" name="email_confirmation" id="email-confirm"
-                                       aria-describedby="emailHelp" placeholder="Confirm email">
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with
-                                                                                   anyone else.
-                                </small>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control" name="password" id="password"
-                                       placeholder="Password">
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" name="password_confirmation"
-                                       id="password-confirm" placeholder="Confirm password">
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
-                    </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control" name="email" id="email"
+                                aria-describedby="emailHelp" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" name="email_confirmation" id="email-confirm"
+                                aria-describedby="emailHelp" placeholder="Confirm email">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with
+                                anyone else.
+                            </small>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="password" id="password"
+                                placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="password_confirmation"
+                                id="password-confirm" placeholder="Confirm password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
-	</div>
-	
-	
+    </div>
+</div>
 @endsection
